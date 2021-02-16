236cd05965539c874eaf5383636be071ce1c11a0
Madhav Anand <madhav1004@icloud.com>
2021-02-16 20:38:14 +0530

    declared typescript and changed next.config.json for images like svg.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import styles from '../../styles/Home.module.scss';
 
 export default function Home() {
   return (
@@ -16,7 +16,7 @@ export default function Home() {
 
         <p className={styles.description}>
           Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          <code className={styles.code}>pages/index.tsx</code>
         </p>
 
         <div className={styles.grid}>
