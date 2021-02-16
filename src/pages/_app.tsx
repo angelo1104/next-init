da9ddd3a61fe07e3859098e84c62f35fefd91243
Madhav Anand <madhav1004@icloud.com>
2021-02-16 21:21:42 +0530

    prettier and eslitn initialized.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,8 @@
-import '../../styles/globals.scss'
-import {AppProps} from "next/app";
+import "../../styles/globals.scss";
+import { AppProps } from "next/app";
 
-function MyApp({ Component, pageProps }:AppProps):JSX.Element {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
